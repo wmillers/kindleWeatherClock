dee3b0263945ab7524007fc8a1493efa2c80e4ec
wmillers <31211860+wmillers@users.noreply.github.com>
2020-12-28 19:05:10 +0800

    v1.5.16 blive beta

--- blivedm/startBliveServer.py
+++ blivedm/startBliveServer.py
@@ -134,7 +134,7 @@ class MyBLiveClient(blivedm.BLiveClient):
         aprint(f'<big><b>{message.username}</b> 购买 <b>{message.gift_name}</b></big>')
 
     async def _on_super_chat(self, message: blivedm.SuperChatMessage):
-        aprint(f'<b>SuperChat ¥{message.price}|</b> {message.uname}: <b>{message.message}</b>')
+        aprint(f'<b>SuperChat ¥{message.price}</b>| {message.uname}: <b>{message.message}</b>')
 
 
 async def initDm(room_id):
