eedf3a44a111e21b92e8805c1c1067380a69c4cd
wmillers <31211860+wmillers@users.noreply.github.com>
2021-08-06 02:21:33 +0800

    self-resume after blive upgrade

--- blivedm/startBliveServer.py
+++ blivedm/startBliveServer.py
@@ -32,13 +32,13 @@ class Resquest(BaseHTTPRequestHandler):
         res=cmd_res+('<br>' if cmd_res and needExtra else '')
         if needExtra:
             count=0
-            while count<15/.2:
+            while count<15/.1:
                 count+=1
                 danmu=readFromLive()
                 if (danmu and danmu!='<br>'):
                     res=res+danmu
                     break
-                sleep(.2)
+                sleep(.1)
         return self.wfile.write((res if res.strip() else '\n').encode('utf-8'))
 
     def do_POST(self):
