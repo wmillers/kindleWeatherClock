157f636a529e687f6b14daf512353a80153fd9cd
wmillers <31211860+wmillers@users.noreply.github.com>
2021-08-30 16:46:07 +0800

    add liveRanger to auto resume live play

--- blivedm/startBliveServer.py
+++ blivedm/startBliveServer.py
@@ -96,7 +96,7 @@ def corsAccess(url, data=None, method=None):
         return ''
 
 def controlRoom(path, data=None, method=None):
-    global new_room_id, que, info, status_code, last_room_id, status
+    global control_code, que, info, status_code, status
     ori_cmd='?'.join(path.split('?')[1:])
     cmd=ori_cmd.lower()
     if cmd.find('cors:')!=-1:
@@ -109,10 +109,16 @@ def controlRoom(path, data=None, method=None):
         needExtra=True
     elif (str(cmd).isdigit()):
             room_id=int(cmd)
-            if (room_id>0 and room_id<999999999999):
+            if (room_id>0 and room_id<1e15):
                 res='[RECV] Room<b>'+cmd+'</b>'
-                new_room_id.value=room_id
-                info['pop']=1
+                if (info['room_id']!=room_id):
+                    print('[kill] room id: '+str(info['room_id']))
+                    info['room_id']=room_id
+                    info['super_chat']=[]
+                    info['pop']=0
+                    control_code.value=room_id
+                else:
+                    print('[recv] but same')
             elif room_id==0:
                 res='[RECV] Room keeps'
             else:
@@ -121,26 +127,25 @@ def controlRoom(path, data=None, method=None):
         if (cmd=='history'):
             res='<br>'.join(history)
         elif (cmd=='bye'):
-            new_room_id.value=-1
+            control_code.value=-1
             res='[DEACTIVATE] client request turn off'
         elif (cmd=='kick'):
-            new_room_id.value=-2
+            control_code.value=-2
             info['pop']=1
             res='[SLEEP] & [KCIK] restart OK'
         elif (cmd=='upgrade'):
-            new_room_id.value=-3
+            control_code.value=-3
             info['pop']=1
             res='[UPGRADE] it depends on network'
         elif (cmd=='status'):
             info['que_size']=que.qsize()
             if (status_code.value!=0 and que.qsize()>5):
                 status_code.value=0
-            info['status_code']=status_code.value
-            info['status']=status[status_code.value]
-            if (info['room_id']!=last_room_id.value):
-                info['room_id']=last_room_id.value
-                info['super_chat']=[]
-                info['pop']=0
+            if (info['status_code']!=status_code.value):
+                info['status_code']=status_code.value
+                info['status']=status[status_code.value]
+                if (info['status_code']==2):
+                    info['room_id']=0
             res=info
         elif (cmd=='clients'):
             res=clients
@@ -202,9 +207,10 @@ def readFromLive(timeout=5):
     return res
 
 
-def initServer(r, c1, c2, c3):
-    global que, new_room_id, status_code, last_room_id
-    que, new_room_id, status_code, last_room_id=r, c1, c2, c3
+def initServer(q, r, c, s):
+    global que, control_code, status_code, info
+    que, control_code, status_code=q, c, s
+    info['room_id']=r
     host = ('localhost', 8099)
     print("Starting server, listen at: %s:%s" % host, flush=True)
     server = ThreadedHTTPServer(host, Resquest)
@@ -214,7 +220,7 @@ def initServer(r, c1, c2, c3):
         print('[initServer:8099] '+repr(e), flush=True)
     else:
         print('[initServer:8099] normal exit', flush=True)
-    new_room_id.value=-1
+    control_code.value=-1
 
 
 def aprint(a):
@@ -248,7 +254,7 @@ class MyBLiveClient(blivedm.BLiveClient):
         price=round(gift.total_coin/1e3)
         if (gift.coin_type!='silver' and price>=5):# gift.num
             identity=supbold(' ᴀʙᴄ'[gift.guard_level] if gift.guard_level else '')
-            aprint(bigbold(f'{identity}{gift.uname} 赠送{gift.gift_name}x{gift.num}#{price}', .64+math.pow(price, 1/3)/20))
+            aprint(bigbold(f'{identity}{gift.uname} 赠送{gift.gift_name}x{gift.num}#{price}', .64+max(math.pow(price, 1/3)/40, 1/(1+math.pow(math.e, -.002*price+3))))) # (936, .24)
     async def _on_buy_guard(self, message: blivedm.GuardBuyMessage):
         aprint(f'{bigbold(message.username)} 成为 {bigbold(message.gift_name)}#{round(message.price/1e3)}')
 
@@ -300,59 +306,55 @@ def main():
     print('--- START at '+ctime()+' ---\n--- '+sys.path[0]+' ---')
     os.chdir(sys.path[0])
     que = Queue()
-    new_room_id=Value(ctypes.c_longlong, 0)
+    control_code=Value(ctypes.c_longlong, 0)
     status_code=Value(ctypes.c_int, 0)
-    last_room_id=Value(ctypes.c_longlong, 0)
-    p = Process(target=initServer, args=(que,new_room_id,status_code,last_room_id,))
-    p.start()
+    room_id=0
     c=False
-    isOn=True
     if (len(sys.argv)>1):
-        last_room_id.value=int(sys.argv[1])
-        c = Process(target=runDm, args=(que,last_room_id.value,))
+        room_id=int(sys.argv[1])
+        c = Process(target=runDm, args=(que,room_id,))
         c.start()
-        print('[init] preset room_id: '+str(last_room_id.value), flush=True)
-        que.put_nowait('[INIT] new room: '+str(last_room_id.value))
+        print('[init] preset room_id: '+str(room_id), flush=True)
+        que.put_nowait('[INIT] new room: '+str(room_id))
     else:
         print('[wait] No preset room id, wait for client request', flush=True)
         setSleep(que, status_code, 1)
+    p = Process(target=initServer, args=(que,control_code,status_code,room_id,))
+    p.start()
+    isOn=True
     while isOn:
         if (status_code.value==0 and que.qsize()>5000):
             print('[sleep] blive off, request room_id to wake up')
             kill(c)
-            last_room_id.value=0
             clear_que(que, 500)
             setSleep(que, status_code, 2)
-        if (new_room_id.value!=0):
-            if (new_room_id.value<0):
-                if (new_room_id.value==-1):
+        if (control_code.value!=0):
+            if (control_code.value<0):
+                if (control_code.value==-1):
                     print('[deactivate] goodbye')
                     isOn=False
-                elif (new_room_id.value==-2):
+                elif (control_code.value==-2):
                     print('[kick&kill] restart script '+str(sys.argv))
                     setSleep(que, status_code, 3)
                     isOn=False
-                    last_room_id.value=0
                     kill(p)
                     os.execv(sys.executable, ['python3'] + sys.argv)
-                elif (new_room_id.value==-3):
+                elif (control_code.value==-3):
                     print('[upgrade] it takes a while')
                     status_code.value=4
                     try:
                         subprocess.run('/bin/bash updateBlive.sh', shell=True, executable="/bin/bash")
                     except Exception as e:
                         que.put_nowait(repr(e))
-            elif (new_room_id.value!=last_room_id.value):
-                if (last_room_id.value!=0):
-                    print('[kill] room id: '+str(last_room_id.value))
-                    kill(c)
-                    clear_que(que)
-                last_room_id.value=new_room_id.value
-                print('[launch] new room id: '+str(last_room_id.value))
+            else:
+                kill(c)
+                clear_que(que)
+                room_id=control_code.value
+                print('[launch] new room id: '+str(room_id))
                 status_code.value=0
-                c = Process(target=runDm, args=(que,last_room_id.value,))
+                c = Process(target=runDm, args=(que,room_id,))
                 c.start()
-            new_room_id.value=0
+            control_code.value=0
         sleep(.1)
     print('***  END  at '+ctime()+' ***', flush=True)
     os._exit()
